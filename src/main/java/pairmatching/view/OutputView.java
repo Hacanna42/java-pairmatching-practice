ea6daa5ced49284af58aeeef7d4a3c0501a8289a
Hacanna42 <dtmdgus901@gmail.com>
2024-12-10 21:16:14 +0900

    feat(OutputView): 전체 현황 출력 기능 추가

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,4 +1,28 @@
 package pairmatching.view;
 
+import java.util.List;
+import pairmatching.domain.Course;
+import pairmatching.domain.Level;
+import pairmatching.domain.Mission;
+
 public class OutputView {
+    public static void printOverallStatus() {
+        System.out.println("#############################################");
+        printCourses();
+        printMissions();
+        System.out.println("############################################");
+    }
+
+    private static void printCourses() {
+        System.out.println("과정: " + String.join(" | ", Course.getStringValues()));
+    }
+
+    private static void printMissions() {
+        System.out.println("미션:");
+        for (Level level : Level.values()) {
+            System.out.printf("- %s: ", level.getName());
+            List<String> missionNamesOfLevel = Mission.getNamesFrom(level);
+            System.out.println(String.join(" | ", missionNamesOfLevel));
+        }
+    }
 }
