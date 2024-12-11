b84c8e40ed2a04fff24115308b64648031751551
Hacanna42 <dtmdgus901@gmail.com>
2024-12-11 22:11:49 +0900

    feat: 각종 분기에 대한 출력 기능 추가

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,7 +1,6 @@
 package pairmatching.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Scanner;
 import pairmatching.domain.Function;
 import pairmatching.dto.SelectedTargetDto;
 import pairmatching.util.Parser;
@@ -17,10 +16,13 @@ public class InputView {
     }
 
     public static SelectedTargetDto selectTargetMission() {
-        // Project Structrue SDK 관련 버그로 인해, Console.readLine() 이 아닌 Java 기본의 Scanner.nextLine()을 사용함.
         System.out.println("과정, 레벨, 미션을 선택하세요.\nex) 백엔드, 레벨1, 자동차경주");
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        return Parser.makeSearchRequestDto(input);
+        return Parser.makeSearchRequestDto(Console.readLine());
+    }
+
+    public static boolean askForceMatching() {
+        System.out.println("매칭 정보가 있습니다. 다시 매칭하시겠습니까?");
+        System.out.println("네 | 아니오");
+        return Console.readLine().equals("네");
     }
 }
--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -4,13 +4,32 @@ import java.util.List;
 import pairmatching.domain.Course;
 import pairmatching.domain.Level;
 import pairmatching.domain.Mission;
+import pairmatching.domain.Pair;
 
 public class OutputView {
     public static void printOverallStatus() {
+        System.out.println();
         System.out.println("#############################################");
         printCourses();
         printMissions();
         System.out.println("############################################");
+    }
+
+    public static void printMatchingResult(List<Pair> matchedPairs) {
+        System.out.println();
+        System.out.println("페어 매칭 결과입니다.");
+        for (Pair pair : matchedPairs) {
+            System.out.println(pair.toString());
+        }
+        System.out.println();
+    }
+
+    public static void printErrorMessage(String message) {
+        System.out.println("[ERROR] " + message);
+    }
+
+    public static void printClearPairMessage() {
+        System.out.println("초기화 되었습니다.");
     }
 
     private static void printCourses() {
